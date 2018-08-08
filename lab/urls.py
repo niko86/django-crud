a1a5f354a0211c08311609fc15514c585cd5f660
Ian Nicholson <me@niko86.com>
2018-08-08 15:03:47 +0100

    Modified basic views with some bootstrap prettiness and other changes

--- lab/urls.py
+++ lab/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from . import views
+from lab.views import AnotherView, IndexView
 
 app_name = 'lab'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', IndexView.as_view(), name='index'),
+    path('another', AnotherView.as_view(), name='another'),
     #path('<int:question_id>/', views.detail, name='detail'),
     #path('<int:question_id>/results/', views.results, name='results'),
     #path('<int:question_id>/vote/', views.vote, name='vote'),
--- lab/views.py
+++ lab/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
-def index(request):
-    return render(request, 'lab/index.html', {'message': 'Hello World'})
+class IndexView(View):
+
+    def get(self, request):
+        return render(request, 'lab/index.html', {'message': 'Hello World'})
+
+class AnotherView(View):
+
+    def get(self, request):
+        return render(request, 'lab/another.html')
