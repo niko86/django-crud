25538d344820b958af7af29c5de69d9bd9a7877d
Ian Nicholson <inicholson@solmek.com>
2018-08-22 01:20:14 +0100

    some more changes, still not working yet on heroku

--- crud/settings.py
+++ crud/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,5 +134,4 @@ STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Configure Django App for Heroku.
-import django_heroku
 django_heroku.settings(locals())
