0875783b9a90b7b07f089e1d2e0f9c5d34b4ffab
Ian Nicholson <me@niko86.com>
2018-08-22 12:02:38 +0100

    making atterberg list the main item and at the root directory for now

--- atterberg/urls.py
+++ atterberg/urls.py
@@ -5,8 +5,8 @@ from atterberg import views
 app_name = 'atterberg'
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('atterbergs', views.AtterbergList.as_view(), name='atterberg-list'),
+    #path('', views.IndexView.as_view(), name='index'),
+    path('', views.AtterbergList.as_view(), name='atterberg-list'),
     path('atterberg/add/', views.AtterbergCreate.as_view(), name='atterberg-add'),
     path('atterberg/<uuid:pk>/xml', views.download_xml, name='atterberg-xml'),
     path('atterberg/<uuid:pk>/edit/',views.AtterbergUpdate.as_view(), name='atterberg-edit'),
--- crud/urls.py
+++ crud/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('lab/', include('lab.urls')),
-    path('att/', include('atterberg.urls')),
+    path('/', include('atterberg.urls')),
 ]
