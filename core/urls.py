78c6ac671eb7c51b36e0b0e467b48e938e37a232
Ian Nicholson <me@niko86.com>
2018-09-13 15:58:35 +0100

    one more go at favicon

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
@@ -7,6 +7,7 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    re_path(r'^favicon.png$', RedirectView.as_view(url='/static/favicon/favicon.png')),
     path('login/', views.LoginFormView.as_view(), name='login'),
     path('logout/', views.LogoutFormView.as_view(), name='logout'),
 ]
